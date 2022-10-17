ac1164fd83625225f7b03e885117e12252a3ece6
hasan <hasanakoz1986@gmail.com>
2022-10-17 23:21:03 +0300

    sample fetch with useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
+import User from "./components/User";
 import UseEffectHook from "./UseEffectHook";
 
 function App() {
   const [show, setShow] = useState(true);
   return (
     <div className="container text-center mt-4">
-      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
         {show ? "Hide" : "Show"}
       </button>
       {show && <LifeCycleMethods />}
-      {show && <UseEffectHook />}
+      {show && <UseEffectHook />} */}
+      <User />
     </div>
   );
 }
